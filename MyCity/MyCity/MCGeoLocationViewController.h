2f4aa37afde25e70ff5df04667d02556c7ecc7ac
Cui Wei <cuiwei.exe@gmail.com>
2013-07-17 22:29:59 +0800

    add info

--- MyCity/MyCity/MCGeoLocationViewController.h
+++ MyCity/MyCity/MCGeoLocationViewController.h
@@ -15,10 +15,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *CityNameTag;
 @property (weak, nonatomic) IBOutlet MKMapView *MapView;
 
-
 @property NSMutableArray *GeoLocationInfo;
 @property NSString *currentCityName;
 
+- (IBAction)showAddedCoordinates:(id)sender;
 - (IBAction)addGeoInfo:(id)sender;
 
 -(void)setCityName:(NSString*)cityName;
