121f8a829fce77265f4cc2dee9ff3f80f8f64dee
Cui Wei <cuiwei.exe@gmail.com>
2013-07-22 17:31:52 +0800

    add explorer

---
+++ MyCity/MyCity/MCRandomLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCRandomLocationViewController.h
+//  MyCity
+//
+//  Created by Cui Wei on 22/7/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCRandomLocationViewController : UIViewController
+
+@end
---
+++ MyCity/MyCity/MCRandomLocationViewController.m
@@ -0,0 +1,38 @@
+//
+//  MCRandomLocationViewController.m
+//  MyCity
+//
+//  Created by Cui Wei on 22/7/13.
+//
+//
+
+#import "MCRandomLocationViewController.h"
+
+@interface MCRandomLocationViewController ()
+
+@end
+
+@implementation MCRandomLocationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
